a5de0cb2a11e50eb49dbe3b563d8d2a26ab26657
Amy Siripoonya <aps91@cam.ac.uk>
2024-06-14 16:28:53 +0100

    case statements for multiple operators

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const readline = require('readline-sync');
+
 console.log('Welcome to the calculator!');
+console.log('==========================')
+console.log('Enter the operator: ');
+const op = readline.prompt();
+console.log('Enter the first number: ');
+const n1 = parseInt(readline.prompt());
+console.log('Enter the second number: ');
+const n2 = parseInt(readline.prompt());
+
+let ans;
+
+switch (op) {
+    case "+":
+        ans = n1 + n2;
+        break;
+    case "-":
+        ans = n1 - n2; 
+        break;
+    case "*":
+        ans = n1 * n2;
+        break;
+    case "/":
+        ans = n1 / n2;
+        break;
+}
+
+console.log(ans);
